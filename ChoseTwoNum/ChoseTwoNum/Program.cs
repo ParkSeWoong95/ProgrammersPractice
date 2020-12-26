d95fc25748d8860b7ca1e93adc2850cb63356829
ParkSeWoong <atpdnd@gmail.com>
2020-12-26 20:45:28 +0900

    test

--- ChoseTwoNum/ChoseTwoNum/Program.cs
+++ ChoseTwoNum/ChoseTwoNum/Program.cs
@@ -11,7 +11,7 @@ namespace ChoseTwoNum
             Plustwonum.solution(new int[] { 2, 1, 3, 4, 1 });
 
             int[] numbers = new int[5] { 2, 1, 3, 4, 1 };  // 메인함수에서 number를 입력받게해서 유동적으로 하고 싶습니다.
-
+            //TEST
         }
     }
 }
