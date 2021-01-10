e72545c474b6d8acc826e8de8aa9060cf91e091b
ParkSeWoong <atpdnd@gmail.com>
2021-01-10 18:46:16 +0900

    string이 처음부터 시작해서 마지막 -4 까지 '*'

---
+++ 핸드폰 번호 가리기/핸드폰 번호 가리기/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace 핸드폰_번호_가리기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solution.solution("01051904199");
+            Solution.solution("027778888");
+        }
+    }
+}
---
+++ 핸드폰 번호 가리기/핸드폰 번호 가리기/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 핸드폰_번호_가리기
+{
+    class Solution
+    {
+        public static string solution(string phone_number)
+        {
+            string answer = "";
+            // char[] x = phone_number.ToCharArray();
+
+            for (int i = 0; i < phone_number.Length; i++)
+            {
+                if (i < phone_number.Length - 4)
+                {
+                    answer += '*';
+                }
+                else
+                {
+                    answer += phone_number[i];
+                }
+            }
+            return answer;
+        }
+    }
+}
